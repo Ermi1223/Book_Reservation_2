a5075f840a939666934d6270c21fac6d5551176b
Ermiyas <ermi1223b@gmail.com>
2024-11-28 01:38:32 -0800

    Initial commit

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    presets: [
+      '@babel/preset-env',
+      '@babel/preset-react' // If you're using React
+    ],
+    plugins: [
+      '@babel/plugin-transform-runtime'
+    ]
+  };
+
+  
+  

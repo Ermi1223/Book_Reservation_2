bbb219355457f6e68e5c265da11f8e5b3e34ad0f
Ermiyas <ermi1223b@gmail.com>
2024-11-28 03:48:04 -0800

    CI test

--- jest.setup.js
+++ jest.setup.js
@@ -9,18 +9,3 @@ module.exports = {
   
   
   
-  module.exports = {
-  testEnvironment: "node",
-  reporters: [
-    "default",
-    [
-      "jest-junit",
-      {
-        outputDirectory: "./test-results",
-        outputName: "results.xml",
-      },
-    ],
-  ],
-};
-
-  
